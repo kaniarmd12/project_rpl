6d5a9a7f64553225bd294939a4542db5e7fd8fc9
eka-wariah <ekawariah877@gmail.com>
2024-06-01 23:19:43 +0700

    membuat migration

---
+++ database/migrations/2024_06_01_160055_create_bags_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bags', function (Blueprint $table) {
+            $table->bigIncrements('bag_id');
+            $table->unsignedBigInteger('bag_brand_id');
+            $table->unsignedBigInteger('bag_category_id');
+            $table->bigInteger('bag_price');
+            $table->bigInteger('bag_stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bags');
+    }
+};
